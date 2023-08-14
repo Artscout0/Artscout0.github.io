51084e7ed5cafe999e7b8e1e3a2ef68360e34a50
TomassTribis <132785458+Artscout0@users.noreply.github.com>
2023-08-14 13:03:59 +0200

    initial commit

---
+++ donations.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--
+	Project: Multimedia project, that doubles as the Tomass is Always Right Foundation website
+	Author: Tomass Tribis
+	Date: V1 19/1/2023, with another version every week since. Last version on 9/3/23
+-->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="The Tomass is Always Right Foundation">
+    <meta name="keywords" content="Right, Left, Meme, Memes, joke, Prank, US President, Foundation, fdn, Tax evasion, School Project, School, Project, [A-Za-z]{0,15}">
+    <meta name="author" content="Tomass Tribis">
+    <title> Full donation list | The Tomass is Always Right Foundation</title>
+    <link rel="shortcut icon" href="img/Logo.svg" type="image/svg">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+
+<body>
+    <header>
+        <h1>Donations</h1>
+        <p>Here, you see how much money has been donated <br> This might have errors, due to me not being familiar enough with PHP.</p>
+        <nav>
+            <ul>
+                <li>
+                    <a href="index.html">Welcome</a>
+                </li>
+                <li>
+                    <a href="page2.html">Donate</a>
+                </li>
+                <li>
+                    <a href="#">Donations</a>
+                </li>
+                <li>
+                    <a href="hire.html">Hiring</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <? 
+        include "phpFormResponse/table.php"
+        ?>
+    </main>
+    <footer>
+        <h2>
+            Links to social media:
+        </h2>
+        <ul style="list-style: none;">
+            <li>
+                <a href="https://youtube.com/@Artscout">
+                    <div class="youtube"></div>
+                    <p>Youtube</p>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/profile.php?id=100089907003128">
+                    <div class="facebook"></div>
+                    <p>Facebook</p>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com/ihaventplayedeldenringyet/">
+                    <div class="instagram"></div>
+                    <p>Instagram</p>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/tomass-tribis-33abb6269/">
+                    <div class="linkedin"></div>
+                    <p>Linkedin</p>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/TomassTribis">
+                    <div class="twitter"></div>
+                    <p>Twitter</p>
+                </a>
+            </li>
+        </ul>
+    </footer>
+</body>
+
+</html>
